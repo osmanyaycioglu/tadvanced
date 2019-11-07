0dd88f99a0daba87e37a5b7bcbdd85b432fb1f09
Osman <osman.yaycioglu@gmail.com>
2019-11-07 12:04:11 +0300

    Day 4 b

---
+++ TurkcelJava/src/com/turkcell/training/rxjava/Customer.java
@@ -0,0 +1,28 @@
+package com.turkcell.training.rxjava;
+
+public class Customer {
+	private String isim;
+	private String soyisim;
+	private String tel;
+	public String getIsim() {
+		return isim;
+	}
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	public String getSoyisim() {
+		return soyisim;
+	}
+	public void setSoyisim(String soyisim) {
+		this.soyisim = soyisim;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+}
---
+++ TurkcelJava/src/com/turkcell/training/rxjava/PrintService.java
@@ -0,0 +1,9 @@
+package com.turkcell.training.rxjava;
+
+public class PrintService {
+	
+	public void print(Employee emp) {
+		System.out.println(emp.getName() + " " + emp.getSurname());
+	}
+	
+}
